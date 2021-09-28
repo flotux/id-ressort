229889cc96decb6051d55612d2e341fd90b240c7
florian vitalli <flvitalli@gmail.com>
2021-09-28 22:09:13 +0200

    ajout de la fonction reset

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 
-var step = false;
 
-function haveNoStep() {
-    element = document.getElementById("windingStep");
-    element.style.visibility = "visible";
-    element.style.height = "auto";
-    step = true;
-}
-
-function haveStep() {
-    element = document.getElementById("windingStep");
-    element.style.visibility = "hidden";
-    element.style.height = 0;
-    step = false;
-}
-
-
-function sensorFeedCalculation() {
+function sensorFeedCalculation(reset = false) {
 
     const springDiam = document.getElementById("springDiam");
     const diamTolerance = document.getElementById("diamTolerance");
     const numberOfCoils = document.getElementById("numberOfCoils");
+
+    if (reset) {
+        document.getElementById("heroResult").style.visibility = "hidden";
+        springDiam.value = "";
+        diamTolerance.value = "";
+        numberOfCoils.value = "";
+        return;
+    }
  
     if (numberInputValidation(springDiam, diamTolerance, numberOfCoils)) {
         var sensorFeed = Math.floor(diamTolerance.value*2*Math.PI*numberOfCoils.value*0.80);
@@ -28,9 +20,15 @@ function sensorFeedCalculation() {
             sensorFeed = Math.floor(springDiam.value*0.75);        
         }
 
-        document.getElementById("sensorFeedResult").innerHTML = "Plage : " + sensorFeed + "mm";
+        document.getElementById("sensorFeedResult").innerHTML = "Plage de palpage : " + sensorFeed + "mm";
         document.getElementById("heroResult").style.visibility = "visible";
+    } else {
+        document.getElementById("heroResult").style.visibility = "hidden";
     }
+}
+
+function clearInputs() {
+    sensorFeedCalculation(true);
 }
 
 
