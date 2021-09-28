27557c0b845d6a7dfa563fe6c3f033a2f60b1c86
florian vitalli <flvitalli@gmail.com>
2021-09-28 21:30:54 +0200

    ajout du ficher validity.js pour factorisation

--- main.js
+++ main.js
@@ -33,25 +33,4 @@ function sensorFeedCalculation() {
     }
 }
 
-/**
- * check the validity of numbers inputs.
- * call a function to inlight then if they value is 0.
- * @param  {[object]} Element [numbers inputs DOM elements]
- * @return {[bool]}     true : if all the param is valid number.
- * 
- */
-function numberInputValidation(...inputs) {  
-    var paramValidity = true;
-    inputs.forEach(element => {
-       if (!element.value || element.value == 0) {
-           element.classList.add("is-danger");
-           element.value="";
-           element.placeholder='Entrer un nombre sup. à 0';
-           paramValidity = false;
-       } else {
-        element.classList.remove("is-danger");
-       }
-    });
 
-    return paramValidity;
-}
