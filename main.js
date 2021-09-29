8386d8342547caae948b1c264bdddafc4859122c
florian vitalli <flvitalli@gmail.com>
2021-09-29 19:52:20 +0200

    refactoring

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 
 function sensorFeedCalculation(clear = false) {
 
+    if (clear) {
+        document.getElementById("heroResult").style.visibility = "hidden";
+        document.getElementById("sensorSpringDiam").value = "";
+        diamTolerance = document.getElementById("sensorDiamTolerance").value = "";
+        const numberOfCoils = document.getElementById("sensorNumberOfCoils").value = "";
+        return;
+    }
+
     const springDiam = document.getElementById("sensorSpringDiam");
     const diamTolerance = document.getElementById("sensorDiamTolerance");
     const numberOfCoils = document.getElementById("sensorNumberOfCoils");
 
-    if (clear) {
-        document.getElementById("heroResult").style.visibility = "hidden";
-        springDiam.value = "";
-        diamTolerance.value = "";
-        numberOfCoils.value = "";
-        return;
-    }
- 
     if (numberInputValidation(springDiam, diamTolerance, numberOfCoils)) {
         var sensorFeed = Math.floor(diamTolerance.value*2*Math.PI*numberOfCoils.value*0.80);
-        if (sensorFeed > (springDiam.value*0.75)) {
+        if (sensorFeed > (springDiam.value*Math.PI*0.75)) {
             sensorFeed = Math.floor(springDiam.value*0.75);        
         }
 
